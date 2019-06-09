2fe22b83aacfda7798ee5768bb6c987bf4e0e62e
victorkaplunov <victorkaplunov@yandex.ru>
2019-06-09 13:15:19 +0300

    initial upload

---
+++ flask_app.py
@@ -0,0 +1,100 @@
+from flask import Flask, send_from_directory, url_for
+import os
+import sqlite3
+import json
+
+app = Flask(__name__)
+id_list = []
+l_host = "http://127.0.0.1:5000"
+
+
+@app.route('/')
+def index():
+    return "<html> \
+    <a href=" + url_for('show_vac_calendar', vac_id='30962151') + ">" + url_for('show_vac_calendar', vac_id='30962151') + "</a><br> \
+    <a href = " + url_for('show_vac_description', vac_id='30962151') + " > " + url_for('show_vac_description', vac_id='30962151') + "</a><br>\
+    <a href = " + url_for('show_vac_top_new_by_id') + " > " + url_for('show_vac_top_new_by_id') + "</a><br>\
+    <a href = " + url_for('show_vac_top_new_by_data') + " > " + url_for('show_vac_top_new_by_data') + "</a><br>\
+    <a href=" + url_for('show_vac_of_employer', empl_name='СофтПро') + ">" + \
+    url_for('show_vac_of_employer', empl_name='СофтПро') + \
+    "</a><br></html>"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/vac/cal/<int:vac_id>')
+def show_vac_calendar(vac_id):
+    """Show the publication data of vacancy with the given id"""
+    con = sqlite3.connect("testdb.db")
+    cur = con.cursor()
+    sql = "SELECT data FROM calendar WHERE id = " + str(vac_id)
+    cur.execute(sql)
+    vac = cur.fetchall()
+    con.close()
+    data_list = []
+    for i in vac:
+        data_list.append(i[0])
+    return str(data_list)
+
+
+@app.route('/vac/<int:vac_id>')
+def show_vac_description(vac_id):
+    """Show the description of vacancy with the given id"""
+    con = sqlite3.connect("testdb.db")
+    cur = con.cursor()
+    sql = "SELECT * FROM vacancies WHERE id = " + str(vac_id)
+    cur.execute(sql)
+    vac = cur.fetchone()
+    return str(
+        json.loads(vac[1])['employer']["name"] + "\n" +
+        json.loads(vac[1])["name"] + "\n" +
+        json.loads(vac[1])['description'])
+
+
+@app.route('/vac/last100id')
+def show_vac_top_new_by_id():
+    """Get last 100 vacancies sorted by id"""
+    con = sqlite3.connect("testdb.db")
+    cur = con.cursor()
+    sql = "SELECT * FROM calendar ORDER BY id DESC LIMIT 100;"
+    cur.execute(sql)
+    vac = cur.fetchall()
+    con.close()
+    data_list = []
+    for i in vac:
+        data_list.append('<a href="https://hh.ru/vacancy/' + str(i[0]) + '">' + str(i[0]) + '</a>')
+    return str(data_list)
+
+
+@app.route('/vac/last100data')
+def show_vac_top_new_by_data():
+    """Get last 100 vacancies sorted by last publication data"""
+    con = sqlite3.connect("testdb.db")
+    cur = con.cursor()
+    sql = "SELECT * FROM calendar ORDER BY data DESC LIMIT 100;"
+    cur.execute(sql)
+    vac = cur.fetchall()
+    con.close()
+    data_list = []
+    for i in vac:
+        data_list.append('<a href="https://hh.ru/vacancy/' + str(i[0]) + '">' + str(i[0]) + '</a>')
+    return str(data_list)
+
+
+@app.route('/empl/<empl_name>')
+def show_vac_of_employer(empl_name):
+    """Get vacancies of given employer"""
+    con = sqlite3.connect("testdb.db")
+    cur = con.cursor()
+    sql = 'SELECT * FROM vacancies WHERE json LIKE "%{}%";'.format(empl_name)
+    cur.execute(sql)
+    vac = cur.fetchall()
+    con.close()
+    data_list = []
+    for i in vac:
+        data_list.append('<a href="https://hh.ru/vacancy/' + str(i[0]) + '">' + str(i[0]) + '</a>')
+    return str(data_list)
---
+++ request.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import sqlite3
+import unicodedata
+import re
+
+con = sqlite3.connect("testdb.db")  # Open database
+cur = con.cursor()  # Create cursor
+
+base_url = 'https://api.hh.ru/'
+api_method = 'vacancies/'
+search_string = \
+                '?text=&' \
+                'no_magic=true&' \
+                'order_by=publication_time&' \
+                'area=1&specialization=1.117&' \
+                'search_field=description&' \
+                'search_field=name&' \
+                'page=0'
+
+req = requests.get(base_url + api_method + search_string)
+pages = req.json()["pages"]
+proxies = {
+    "http": "http://127.0.0.1:8888",
+    "https": "http://127.0.0.1:8888",
+}
+
+# Get list of id from "vacancies" table
+vac_id_list = []
+sql = "SELECT id FROM vacancies"
+try:
+    cur.execute(sql)
+    # print(type((cur.fetchall()[0])[0]))
+    for i in cur.fetchall():
+        vac_id_list.append(i[0])
+except sqlite3.IntegrityError as err:
+        print("Error: ", err)
+
+
+# Make search request
+def resp(n):
+    return requests.get(base_url + api_method + search_string.replace("page=0", "page=" + str(n))
+                        # , proxies=proxies
+                        )
+
+
+# Get list of vacancies from response and write id and data to "calendar" table.
+def id_list(response):
+    vac_list = response.json()["items"]
+    items = []
+    for i in vac_list:
+        items.append(i["id"])
+        sql = 'INSERT INTO calendar (id, data) VALUES (%d, "%s");' % (int(i["id"]), i["published_at"])
+        print(sql)
+        try:
+            cur.executescript(sql)
+        except sqlite3.IntegrityError as err:
+            print("Error: ", err)
+
+        # If vacancy is new, write description to "vacancies" table.
+        if int(i["id"]) not in vac_id_list:
+            # Get vacancies by ID
+            r = requests.get(base_url + api_method + (i["id"])
+                             # , proxies=proxies
+                             )
+            vac = r.json()
+            del vac["branded_description"]  # Remove description in HTML format
+            d = json.dumps(vac, indent=None, ensure_ascii=False, separators=(', ', ': ', ))
+            d = re.sub(r"'", '', d)  # Remove apostrophes from JSON for SQL request safety
+            d = unicodedata.normalize("NFKD", d)  # Return the normal form for the Unicode string
+            sql_in = "INSERT INTO vacancies (id, json) VALUES (%d, '%s');" % (int(i["id"]), d)
+            print(sql_in)
+            try:
+                cur.executescript(sql_in)
+            except sqlite3.IntegrityError as error:
+                print("Error: ", error)
+    return items
+
+
+for n in range(0, pages):
+    s = id_list(resp(n))
+    # print("Items on page: ", len(set(s)))
+
+# Close database connection
+con.close()
