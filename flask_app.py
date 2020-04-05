b2b94b922f0da6ad0455d8c3f302c7b52f208687
victorkaplunov <victorkaplunov@yandex.ru>
2020-04-05 13:45:40 +0300

    Add favicon

--- flask_app.py
+++ flask_app.py
@@ -24,7 +24,7 @@ def index():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'static/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/vac/cal/<int:vac_id>')
