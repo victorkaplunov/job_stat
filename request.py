232206b73c1956b5f870ccf0aed4853e38de7047
victorkaplunov <victorkaplunov@yandex.ru>
2020-04-08 15:04:43 +0300

    remove comments.

--- request.py
+++ request.py
@@ -25,67 +25,67 @@ proxies = {
     "http": "http://127.0.0.1:8888",
     "https": "http://127.0.0.1:8888",
 }
-#
-# # Get list of id from "vacancies" table
-# vac_id_list = []
-# sql = "SELECT id FROM vacancies"
-# try:
-#     cur.execute(sql)
-#     # print(type((cur.fetchall()[0])[0]))
-#     for i in cur.fetchall():
-#         vac_id_list.append(i[0])
-# except sqlite3.IntegrityError as err:
-#         print("Error: ", err)
-#
-#
-# # Make search request
-# def resp(n):
-#     return requests.get(base_url + api_method + search_string.replace("page=0", "page=" + str(n))
-#                         # , proxies=proxies
-#                         )
-#
-#
-# # Get list of vacancies from response and write id and data to "calendar" table.
-# def id_list(response):
-#     vac_list = response.json()["items"]
-#     items = []
-#     for i in vac_list:
-#         items.append(i["id"])
-#         sql = 'INSERT INTO calendar (id, data) VALUES (%d, "%s");' % (int(i["id"]), i["published_at"])
-#         print(sql)
-#         try:
-#             cur.executescript(sql)
-#         except sqlite3.IntegrityError as err:
-#             print("Error: ", err)
-#
-#         # If vacancy is new, write description to "vacancies" table.
-#         if int(i["id"]) not in vac_id_list:
-#             # Get vacancies by ID
-#             r = requests.get(base_url + api_method + (i["id"])
-#                              # , proxies=proxies
-#                              )
-#             vac = r.json()
-#             del vac["branded_description"]  # Remove description in HTML format
-#             json_dump = json.dumps(vac, indent=None, ensure_ascii=False, separators=(', ', ': ', ))
-#             json_dump = re.sub(r"'", '', json_dump)  # Remove apostrophes from JSON for SQL request safety
-#             json_dump = re.sub(r"’", '', json_dump)  # Remove apostrophes from JSON for SQL request safety
-#             json_dump = re.sub(r"&#39;", '', json_dump)  # Remove apostrophes from JSON for SQL request safety, again
-#             json_dump = unicodedata.normalize("NFKD", json_dump)  # Return the normal form for the Unicode string
-#
-#             cleaner = re.compile('<.*?>')  # Remove HTML tags
-#             json_dump = re.sub(cleaner, '', json_dump)
-#
-#             sql_in = "INSERT INTO vacancies (id, json) VALUES (%d, '%s');" % (int(i["id"]), json_dump)
-#             try:
-#                 cur.executescript(sql_in)
-#             except sqlite3.IntegrityError as error:
-#                 print("Error: ", error)
-#     return items
-#
-#
-# for n in range(0, pages):
-#     s = id_list(resp(n))
-#     print("Items on page: ", len(set(s)))
+
+# Get list of id from "vacancies" table
+vac_id_list = []
+sql = "SELECT id FROM vacancies"
+try:
+    cur.execute(sql)
+    # print(type((cur.fetchall()[0])[0]))
+    for i in cur.fetchall():
+        vac_id_list.append(i[0])
+except sqlite3.IntegrityError as err:
+        print("Error: ", err)
+
+
+# Make search request
+def resp(n):
+    return requests.get(base_url + api_method + search_string.replace("page=0", "page=" + str(n))
+                        # , proxies=proxies
+                        )
+
+
+# Get list of vacancies from response and write id and data to "calendar" table.
+def id_list(response):
+    vac_list = response.json()["items"]
+    items = []
+    for i in vac_list:
+        items.append(i["id"])
+        sql = 'INSERT INTO calendar (id, data) VALUES (%d, "%s");' % (int(i["id"]), i["published_at"])
+        print(sql)
+        try:
+            cur.executescript(sql)
+        except sqlite3.IntegrityError as err:
+            print("Error: ", err)
+
+        # If vacancy is new, write description to "vacancies" table.
+        if int(i["id"]) not in vac_id_list:
+            # Get vacancies by ID
+            r = requests.get(base_url + api_method + (i["id"])
+                             # , proxies=proxies
+                             )
+            vac = r.json()
+            del vac["branded_description"]  # Remove description in HTML format
+            json_dump = json.dumps(vac, indent=None, ensure_ascii=False, separators=(', ', ': ', ))
+            json_dump = re.sub(r"'", '', json_dump)  # Remove apostrophes from JSON for SQL request safety
+            json_dump = re.sub(r"’", '', json_dump)  # Remove apostrophes from JSON for SQL request safety
+            json_dump = re.sub(r"&#39;", '', json_dump)  # Remove apostrophes from JSON for SQL request safety, again
+            json_dump = unicodedata.normalize("NFKD", json_dump)  # Return the normal form for the Unicode string
+
+            cleaner = re.compile('<.*?>')  # Remove HTML tags
+            json_dump = re.sub(cleaner, '', json_dump)
+
+            sql_in = "INSERT INTO vacancies (id, json) VALUES (%d, '%s');" % (int(i["id"]), json_dump)
+            try:
+                cur.executescript(sql_in)
+            except sqlite3.IntegrityError as error:
+                print("Error: ", error)
+    return items
+
+
+for n in range(0, pages):
+    s = id_list(resp(n))
+    print("Items on page: ", len(set(s)))
 
 # Wright languages statistics data to database
 languages_list = ['Java', 'Python', 'JavaScript', 'C#', "PHP", 'C++', 'Ruby', 'Groovy']
