fcca35ab7744ba0b8e0ba36d354fa156ca68bf5a
victorkaplunov <victorkaplunov@yandex.ru>
2020-04-19 13:44:15 +0300

    rm "DROP TABLE  charts;"

--- create_table.py
+++ create_table.py
@@ -6,21 +6,19 @@ con = sqlite3.connect("testdb.db")
 # Create cursor
 cur = con.cursor()
 
-# sql = """CREATE TABLE IF NOT EXISTS vacancies (id INTEGER PRIMARY KEY, json TEXT);"""
-# cur.execute(sql)
-# sql = """
-# CREATE TABLE IF NOT EXISTS calendar
-# (
-#   id INTEGER,
-#   data TEXT,
-#   CONSTRAINT vac_data UNIQUE (id, data)
-# );
-# """
-# cur.execute(sql)
+sql = """CREATE TABLE IF NOT EXISTS vacancies (id INTEGER PRIMARY KEY, json TEXT);"""
+cur.execute(sql)
 
-# cur.execute("DROP TABLE  languages;")
-# cur.execute("DROP TABLE  frameworks;")
-cur.execute("DROP TABLE  charts;")
+sql = """
+CREATE TABLE IF NOT EXISTS calendar
+(
+  id INTEGER,
+  data TEXT,
+  CONSTRAINT vac_data UNIQUE (id, data)
+);
+"""
+cur.execute(sql)
+
 
 sql = """
 CREATE TABLE IF NOT EXISTS charts
