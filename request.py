f98757bc0fb4c801c65d2acf2d6b0cc7a09c8fbd
victorkaplunov <victorkaplunov@yandex.ru>
2020-04-20 21:30:31 +0300

    add commit statement to request.

--- request.py
+++ request.py
@@ -135,5 +135,8 @@ def wright_statistic_to_db(chart_name, param_list):
 
 sql = 'DELETE FROM "charts" WHERE "data" = "Go"';
 cur.execute(sql)
+con.commit()
+vac = cur.fetchall()
+print(vac)
 # Close database connection
 con.close()
