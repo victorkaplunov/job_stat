5e16b5525b24c7b71a38d4141d145a920487d893
victorkaplunov <victorkaplunov@yandex.ru>
2020-09-02 14:31:49 +0300

    fix filtering and add some tining.

--- word_cloud.py
+++ word_cloud.py
@@ -5,39 +5,41 @@ from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
 
-
 con = sqlite3.connect("testdb.db")
 cur = con.cursor()
+
+# Get descriptions from all vacancies
 sql = 'SELECT json FROM vacancies'
-# sql = 'SELECT json FROM vacancies LIMIT 1000'
 cur.execute(sql)
 vac = cur.fetchall()
-cleaner = re.compile('<.*?>')  # Remove HTML tags
+cleaner = re.compile('<.*?>')  # This is RegExp for removing HTML tags
 text = ''
-# Create a list of word
 f = open("word_cloud.txt", "a", encoding="utf-8")
+f.truncate(0)
+
+# Create a list of word and filter some content
 for i in vac:
     description = json.loads(i[0])['description']
-    json_dump = re.sub(cleaner, '', description)
+    json_dump = re.sub(cleaner, '', description)  # Remove HTML tags
     json_dump = json_dump.replace('й', 'й')
+    json_dump = json_dump.replace('Й', 'Й')
     json_dump = json_dump.replace('&quot;', '')
     print(json_dump)
     f.write(json_dump)
-    text += json_dump
 f.close()
 
 f = open("word_cloud.txt", "r", encoding="utf-8")
 text = f.read()
 mask = np.array(Image.open("alfa_channel.png"))
+
 # Create the wordcloud object
 wordcloud = WordCloud(background_color="white",
-                      max_words=1000,
+                      max_words=300,
                       mask=mask,
                       # contour_width=3, contour_color='steelblue',
                       width=800, height=600, margin=0
                       ).generate(text)
 
 image = wordcloud.to_image()
-# image.show()
 wordcloud.to_file('w_cloud.png')
 f.close()
