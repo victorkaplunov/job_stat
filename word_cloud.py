808c154ccd7f552c1cf119ecd9e0d03015165f86
victorkaplunov <victorkaplunov@yandex.ru>
2021-01-08 11:05:06 +0300

    add 2021 year to time series chart

--- word_cloud.py
+++ word_cloud.py
@@ -5,37 +5,37 @@ from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
 
-# con = sqlite3.connect("testdb.db")
-# cur = con.cursor()
-#
-# # Get descriptions from all vacancies
-# sql = 'SELECT json FROM vacancies'
-# cur.execute(sql)
-# vac = cur.fetchall()
-# cleaner = re.compile('<.*?>')  # This is RegExp for removing HTML tags
-# text = ''
-# f = open("word_cloud.txt", "a", encoding="utf-8")
-# f.truncate(0)
-#
-# # Create a list of word and filter some content
-# for i in vac:
-#     description = json.loads(i[0])['description']
-#     json_dump = re.sub(cleaner, '', description)  # Remove HTML tags Опыт настройки
-#     json_dump = json_dump.replace('й', 'й')
-#     json_dump = json_dump.replace('Й', 'Й')
-#     json_dump = json_dump.replace('&quot;', '')
-#     print(json_dump)
-#     f.write(json_dump)
-# f.close()
+con = sqlite3.connect("testdb.db")
+cur = con.cursor()
+
+# Get descriptions from all vacancies
+sql = 'SELECT json FROM vacancies'
+cur.execute(sql)
+vac = cur.fetchall()
+cleaner = re.compile('<.*?>')  # This is RegExp for removing HTML tags
+text = ''
+f = open("word_cloud.txt", "a", encoding="utf-8")
+f.truncate(0)
+
+# Create a list of word and filter some content
+for i in vac:
+    description = json.loads(i[0])['description']
+    json_dump = re.sub(cleaner, '', description)  # Remove HTML tags
+    json_dump = json_dump.replace('й', 'й')
+    json_dump = json_dump.replace('Й', 'Й')
+    json_dump = json_dump.replace('&quot;', '')
+    print(json_dump)
+    f.write(json_dump)
+f.close()
 
 f = open("word_cloud.txt", "r", encoding="utf-8")
 text = f.read()
-mask = np.array(Image.open("alfa_channel.png"))
+mask = np.array(Image.open("C:\\Users\\User\\PycharmProjects\\job_stat\\alpha_channel_1.png"))
 
 # Create the wordcloud object
 wordcloud = WordCloud(background_color="white",
-                      max_words=200,
-                      # mask=mask,
+                      max_words=300,
+                      mask=mask,
                       # contour_width=3, contour_color='steelblue',
                       width=600, height=800, margin=0
                       ).generate(text)
