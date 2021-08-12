22d15b10dd7bd5d093fee8894e9c30cdf608aa2e
victorkaplunov <victorkaplunov@yandex.ru>
2021-08-12 08:11:22 +0200

    Update request.py

--- request.py
+++ request.py
@@ -32,7 +32,7 @@ req = requests.get((base_url + search_string).encode('utf-8'))
 # proxies = {"http": http_proxy, "https": https_proxy}
 
 # Get quantity of pages in responce
-pages = 40  # req.json()["pages"]
+pages = 15  # req.json()["pages"]
 
 
 def resp(n):
