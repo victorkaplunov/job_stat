7bf02662c89468333452a9cd97d0148daf3b36b3
Каплунов Виктор <viktor.kaplunov@gazprombank.ru>
2021-08-15 11:27:44 +0300

    try to fix.

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-/home/clingon/.virtualenvs/myvirtualenv/bin/python /home/clingon/job_stat/run.sh
+cd /home/clingon/job_stat/ || exit
+/home/clingon/.virtualenvs/myvirtualenv/bin/python /home/clingon/job_stat/request.py
