3214a96e7720cdca441654a412f78deed991e78a
Каплунов Виктор <viktor.kaplunov@gazprombank.ru>
2021-08-15 15:49:48 +0300

    make style fix.

--- PythonAnywhereAPI.py
+++ PythonAnywhereAPI.py
@@ -5,28 +5,25 @@ import os
 username = 'clingon'
 TOKEN = os.getenv('PA_TOKEN')
 headers = {'Authorization': f'Token {TOKEN}'}
-prefix = f'https://www.pythonanywhere.com/api/v0/user/{username}/'
-# response = requests.post(prefix, json={'executable': 'bash', "working_directory": 'job_stat'}, headers=headers)
-# print(response.status_code)
-# print(response.text)
+base_url = f'https://www.pythonanywhere.com/api/v0/user/{username}/'
 
 # Get first console ID
-response = requests.get(prefix + 'consoles/', headers=headers)
+response = requests.get(base_url + 'consoles/', headers=headers)
 print(response.json())
 console_id = str(response.json()[0]['id'])
 
 # Send command to first console
 command = {'input': 'cd /home/clingon/job_stat\n git pull --rebase\n'}
 # command = {'input': 'python3 request.py\n'}
-response = requests.post(prefix + f'consoles/{console_id}/send_input/',
+response = requests.post(base_url + f'consoles/{console_id}/send_input/',
                          json=command, headers=headers)
 print('send_input: ', response.status_code)
 print(response.text)
 # Get first webapps name
-response = requests.get(prefix + 'webapps/', headers=headers)
+response = requests.get(base_url + 'webapps/', headers=headers)
 domain_name = response.json()[0]['domain_name']
 print(domain_name)
 
 # Reload first webapps
-response = requests.post(prefix + f'webapps/{domain_name}/reload/', headers=headers)
+response = requests.post(base_url + f'webapps/{domain_name}/reload/', headers=headers)
 print(response.status_code)
