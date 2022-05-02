178877be20d538a2832ec675bcbb3e7be2e2e58f
victorkaplunov <victorkaplunov@yandex.ru>
2022-05-02 16:28:50 +0300

    ad stop list amd more..

--- word_cloud.py
+++ word_cloud.py
@@ -9,7 +9,9 @@ con = sqlite3.connect("testdb.db")
 cur = con.cursor()
 
 # Get descriptions from all vacancies
-sql = 'SELECT json FROM vacancies'
+year = '2020'
+sql = f"""SELECT json FROM vacancies WHERE published_at
+              BETWEEN '{year}-01-01T00:00:00+0300' AND '{year}-12-31T11:59:59+0300';"""
 cur.execute(sql)
 vac = cur.fetchall()
 cleaner = re.compile('<.*?>')  # This is RegExp for removing HTML tags
@@ -24,18 +26,42 @@ for i in vac:
     json_dump = json_dump.replace('й', 'й')
     json_dump = json_dump.replace('Й', 'Й')
     json_dump = json_dump.replace('&quot;', '')
+    stop_list = [
+        'Опыт', 'опыт', "от года", "от одного года", 'тестирования', 'тестовой', 'тест планов', 'тест-планов',
+        'тестовой документации', 'тест кейсы', "тест-кейсы", 'тест кейсов', "тест-кейсов",
+        "тест-кейсы", 'чек листов', 'чек-листов', 'чек листы', 'чек-листы', 'тестовых сценариев',
+        'тестирования ПО', "тестирование программного обеспечения", "по тестированию",
+        'Мы ищем', 'мы ищем', "Мы ждем", "Мы ожидаем", "Чем предстоит", "Вам предстоит", "Вы будете",
+        "Что нужно", 'Будет плюсом', 'будет плюсом', "плюсом будет",
+        "Требования", "Базовые знания", "базовые знания", "Знания", "Знание", "принципов работы",
+        "Навыки работы", "Умение работать", "умение работать",
+        'Мы предлагаем', "Мы предлагаем:", "Что мы", "для сотрудников", "скидки от", "скидки на",
+        "ДМС после", "счет компании", "со стоматологией", "доступности от", "чай", "кофе",
+        "возможности для", "возможности для профессионального роста", "интересные задачи",
+        "есть возможность",
+        "Комфортный офис", "комфортный офис", "уютный офис", "современный офис", "от метро",
+        "центре Москвы",
+        "гибкое начало рабочего дня", 'График работы', 'график работы', "гибкий график",
+        'Оформление по ТК РФ', "Официальное оформление", 'по ТК', "ТК РФ",
+        'по результатам', 'на уровне', "заработная плата", "по итогам", "с первого дня",
+        "испытательного срока",
+        "для нас", "от вас", "если вы", "если ты", "для этого", "мы не", "том числе", "один из",
+        "одного из", "не менее", "не только", "на базе", "на основе"
+    ]
+    for word in stop_list:
+        json_dump = json_dump.replace(word, '')
     print(json_dump)
     f.write(json_dump)
 f.close()
 
 f = open("word_cloud.txt", "r", encoding="utf-8")
 text = f.read()
-mask = np.array(Image.open("C:\\Users\\User\\PycharmProjects\\job_stat\\alpha_channel_1.png"))
+# mask = np.array(Image.open("C:\\Users\\User\\PycharmProjects\\job_stat\\alpha_channel_1.png"))
 
 # Create the wordcloud object
 wordcloud = WordCloud(background_color="white",
                       max_words=300,
-                      mask=mask,
+                      # mask=mask,
                       # contour_width=3, contour_color='steelblue',
                       width=600, height=800, margin=0
                       ).generate(text)
