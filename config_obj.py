33b0fffcc8c900cdfd4ed25f9d303d0978e8a639
Victor Kaplunov <victorkaplunov@yandex.ru>
2024-02-19 10:50:54 +0300

    add substring to stop list.

--- config_obj.py
+++ config_obj.py
@@ -2,7 +2,8 @@ class ConfigObj:
     LOCAL_HOST_BASE_URL = 'http://127.0.0.1:5000'
     BASE_URL = 'http://api.hh.ru/vacancies/'
     YEARS = [2019, 2020, 2021, 2022, 2023, 2024]
-    STOP_LIST = ['Формовщик теста', 'тестомес', 'Тестомес']
+    STOP_LIST = ['Формовщик теста', 'тестомес', 'Тестомес',
+                 'Key Account Manager']
     EXCHANGE_RATES = {'RUR': 1, 'EUR': 86,
                  'USD': 73, 'UAH': 2.58,
                  'KZT': 0.2, 'AZN': 53.03}
