ebd47c0aa646982a74fac7c1a01f09a7c2185787
Victor Kaplunov <victorkaplunov@yandex.ru>
2024-03-08 23:28:48 +0300

    remove unused import.

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Numeric, DateTime
+from sqlalchemy import Column, Integer, String, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
