b40cd6d86d1d3e2012846a240736e569d95ec113
Victor Kaplunov <victorkaplunov@yandex.ru>
2024-03-10 20:33:49 +0300

    rename template.

--- flask_app.py
+++ flask_app.py
@@ -146,7 +146,7 @@ def top_employers():
 def salary_by_category():
     """Salary by category"""
     return render_template(
-        '/candlestick.html',
+        '/candle.html',
         chart_data=utils.get_salary_by_category_data(),
         title='Медианная зарплата в зависимости от упоминания языка.'
     )
