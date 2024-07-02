ef86c4d0317fc0115ddde1293130d87ecc5c310f
Victor Kaplunov <victorkaplunov@yandex.ru>
2024-07-02 14:53:45 +0300

    fix bash command.

--- pythonanywhere_api.py
+++ pythonanywhere_api.py
@@ -26,7 +26,7 @@ pa = PythonAnywhereClient()
 # print(response.text)
 
 console_id = pa.get_first_console_id()
-command = 'clear\n cd /home/clingon/job_stat\n unzip -o testdb.zip\n'
+command = 'clear\n cd /home/clingon/job_stat\n git pull --rebase\n'
 pa.send_command_to_console(console_id=console_id, command=command)
 domain_name = pa.get_first_webapps_name()
 pa.reload_webapps(domain_name=domain_name)
