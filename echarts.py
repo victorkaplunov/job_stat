a446a5d0edbc857ce259da964204b9e6d8b7d54a
Victor Kaplunov <victorkaplunov@yandex.ru>
2024-09-11 12:10:37 +0300

    add sorting by median to EchartBoxplot.

--- echarts.py
+++ echarts.py
@@ -177,34 +177,29 @@ def _get_formatted_salary(salary: int) -> str:
 class EchartBoxplot(BaseChart):
     def _get_data(self) -> (list[list], list):
         """Get data for Boxplot chart."""
-        language_list = list()
-        data_list = []
+        tmp_dict = dict()
         for language in Config.PROGRAM_LANGUAGES:
             salary_list = self.db.find_salary_by_substring_in_description(search_phrase=language)
             if len(salary_list) < 10:
                 continue
-            data_list.append(list(salary_list))
-            language_list.append(language)
-        print(f"{language_list=}")
-        for i in data_list:
-            print(i)
-
-        return data_list, language_list
+            tmp_dict[language] = salary_list
+        # Сортируем по медиане для списка зарплат.
+        sorted_dict = dict(sorted(tmp_dict.items(), key=lambda item: statistics.median(item[1])))
+        return sorted_dict
 
     def _get_options(self) -> str:
         """Get options for Boxplot chart."""
         chart_data = self._get_data()
         chart = (Boxplot(init_opts=opts.InitOpts(width="100%")))
-        # chart.add_dataset()
-        chart.add_xaxis(chart_data[1])
-        chart.add_yaxis('2024 г.', chart.prepare_data(chart_data[0]))
+        chart.add_xaxis(list(chart_data.keys()))
+        chart.add_yaxis('2024 г.', chart.prepare_data(chart_data.values()))
         chart.set_global_opts(xaxis_opts=opts.AxisOpts(type_='value', split_number=2),
                               yaxis_opts=opts.AxisOpts(type_='category'),
                               tooltip_opts=opts.TooltipOpts(
+                                  # formatter='{a}, {b0}<br/>{c}<br/>{@[0]}',
                                   value_formatter=utils.JsCode("(value) => '₽' + (value*1).toFixed(0)"),
                                   trigger_on='mousemove')
                               )
-        chart.set_series_opts(label_opts=opts.LabelOpts(is_show=False))
         chart.reversal_axis()
         chart.options['grid'] = {'left': 70, 'right': 20, 'top': 40}
         return chart.dump_options()
